007a1e3a16d30293aa6f1c57d09ce9069b9dcede
valinfernanda <valinfernanda08@gmail.com>
2021-01-14 18:25:05 +0700

    Tugas ReactJs

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="Parentbox">
+      <div class="Foto">
+        <img src="Mulan2.jpg" />
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">Disney</p>
+        <h1 class="Title">Mulan</h1>
+        <p class="Info">Mulan is the most beautiful figure on Disney</p>
+        <a href="#">Order the sticker</a>
+
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
